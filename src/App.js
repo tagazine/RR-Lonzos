8b1a0c8f15878950ddf19ea4100aeaca7c0ecb6d
Your Namegit config --global user.name Your <you@example.com>
2022-06-17 11:06:37 -0400

    With Bonus

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import React, { Suspense } from "react";
+import { Bars } from "react-loader-spinner";
+import "./App.css";
 
 function App() {
+  const AboutUs = React.lazy(() => import("./components/AboutUs"));
+  const Packages = React.lazy(() => import("./components/Packages"));
+
   return (
     <div className="App">
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense
+          fallback={
+            <Bars
+              type="Bars"
+              color="#FF00FF"
+              height={200}
+              width={200}
+              style={{ margin: "0 auto" }}
+            />
+          }
+        >
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
